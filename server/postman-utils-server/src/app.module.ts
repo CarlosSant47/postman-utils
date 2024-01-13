a4635c731a561224082e36e94ca210477eaa69d6
Carlos Santiago <emci1291@gmail.com>
2024-01-13 13:55:12 -0700

    Add: Se agrega ek archivo de configuracion en YAML, en vez del clasico .env

--- server/postman-utils-server/src/app.module.ts
+++ server/postman-utils-server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PostmanUtilsController } from './postman-utils.controller';
 import { PostmanUtilsService } from './postman-utils.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            load: [configuration],
+        }),
+    ],
     controllers: [PostmanUtilsController],
     providers: [PostmanUtilsService],
 })
